806a7337e83d1331b54bd04ddd34486750516b8d
prajwalbhamere <60645397+prajwalbhamere@users.noreply.github.com>
2023-11-15 16:09:21 +0530

    Update contract.sol

--- LP3/BT/C3/contract.sol
+++ LP3/BT/C3/contract.sol
@@ -1,61 +1,38 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.6.2 <0.9.0;
+pragma solidity ^0.8.8;
 
-contract SimpleBank {
-    uint8 private clientCount;
-    mapping (address => uint) private balances;
-    address public owner;
-
-    // Log the event about a deposit being made by an address and its amount
-    event LogDepositMade(address indexed accountAddress, uint amount);
-
-    // Constructor is "payable" so it can receive the initial funding of 30,
-    // required to reward the first 3 clients
-    constructor() public payable {
-        require(msg.value == 30 ether, "30 ether initial funding required");
-        /* Set the owner to the creator of this contract */
-        owner = msg.sender;
-        clientCount = 0;
+contract StudentData{
+    struct Student{
+        uint256 rollNumber;
+        string name;
+        string department;
     }
 
-    /// @notice Enroll a customer with the bank,
-    /// giving the first 3 of them 10 ether as reward
-    /// @return The balance of the user after enrolling
-    function enroll() public returns (uint) {
-        if (clientCount < 3) {
-            clientCount++;
-            balances[msg.sender] = 10 ether;
+    Student[] student;
+
+    function addStudent(uint256 _rollNumber, string memory _name, string memory _department) public {
+        Student memory newStudent = Student({
+            rollNumber : _rollNumber,
+            name : _name,
+            department : _department
+        });
+        student.push(newStudent);
+    }
+    function getStudent(uint256 _rollNumber) public view returns(string memory, string memory){
+        for(uint256 i=0;i < student.length;i++){
+            Student memory stud = student[i];
+            if(stud.rollNumber == _rollNumber){
+                return (stud.name,stud.department);
+            }
         }
-        return balances[msg.sender];
+        return ("Name not Found","Department Not Found");
+    }
+    function getStudentCount() public view returns (uint256){
+        return student.length;
     }
 
-    /// @notice Deposit ether into bank, requires method is "payable"
-    /// @return The balance of the user after the deposit is made
-    function deposit() public payable returns (uint) {
-        balances[msg.sender] += msg.value;
-        emit LogDepositMade(msg.sender, msg.value);
-        return balances[msg.sender];
-    }
-
-    /// @notice Withdraw ether from bank
-    /// @return remainingBal The balance remaining for the user
-    function withdraw(uint withdrawAmount) public returns (uint remainingBal) {
-        // Check enough balance available, otherwise just return balance
-        if (withdrawAmount <= balances[msg.sender]) {
-            balances[msg.sender] -= withdrawAmount;
-            payable(msg.sender).transfer(withdrawAmount);
-        }
-        return balances[msg.sender];
-    }
-
-    /// @notice Just reads balance of the account requesting, so "constant"
-    /// @return The balance of the user
-    function balance() public view returns (uint) {
-        return balances[msg.sender];
-    }
-
-    /// @return The balance of the Simple Bank contract
-    function depositsBalance() public view returns (uint) {
-        return address(this).balance;
+    //Fallback Function
+    receive() external payable{
+        student.push(Student(7,"MSD","IT"));
     }
 }
